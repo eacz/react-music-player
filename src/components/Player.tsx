a96128415abfad0db69e29a2f49cc23116d9a178
Esteban Canteros <estebanacanteros@gmail.com>
2023-02-12 01:08:15 -0300

    add timeline for song duration

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import useSound from 'use-sound'
 import mp3 from '../assets/all_signs_points_to_lauderdale.mp3'
 import { AiFillPlayCircle, AiFillPauseCircle } from 'react-icons/ai'
 import { BiSkipNext, BiSkipPrevious } from 'react-icons/bi'
 import { IconContext } from 'react-icons'
+import coverPhoto from '../assets/cover_photo.jfif'
 
 const Player = () => {
   const [isPlaying, setIsPlaying] = useState(false)
   const [play, { pause, duration, sound }] = useSound(mp3)
+  const [currTime, setCurrTime] = useState({
+    min: 0,
+    sec: 0,
+  })
+
+  const [seconds, setSeconds] = useState()
+
+  const [time, setTime] = useState({
+    min: 0,
+    sec: 0,
+  })
 
   const playingButton = () => {
     if (isPlaying) {
@@ -19,13 +31,58 @@ const Player = () => {
     }
   }
 
+  useEffect(() => {
+    const sec = duration! / 1000
+    const min = Math.floor(sec / 60)
+    const secRemain = Math.floor(sec % 60)
+    const time = {
+      min: min,
+      sec: secRemain,
+    }
+    setTime(time)
+  })
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound) {
+        setSeconds(sound.seek([]))
+        const min = Math.floor(sound.seek([]) / 60)
+        const sec = Math.floor(sound.seek([]) % 60)
+        setCurrTime({
+          min,
+          sec,
+        })
+      }
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [sound])
   return (
     <div className='component'>
       <h2>Playing Now</h2>
-      <img className='musicCover' src='https://picsum.photos/200/200' />
+      <img className='musicCover' src={coverPhoto} />
       <div>
-        <h3 className='title'>Rubaiyyan</h3>
-        <p className='subTitle'>Qala</p>
+        <h3 className='title'>All Signs Point to Lauderdale</h3>
+        <p className='subTitle'>A Day to Remember</p>
+      </div>
+      <div>
+        <div className='time'>
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {time.min}:{time.sec}
+          </p>
+        </div>
+        <input
+          type='range'
+          min='0'
+          max={duration! / 1000}
+          value={seconds}
+          className='timeline'
+          onChange={(e) => {
+            sound.seek([e.target.value])
+          }}
+        />
       </div>
       <div>
         <button className='playButton'>
